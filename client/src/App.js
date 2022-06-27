2c2cf61cf43cb587e86530bf90ad1f141ef800e4
nitesh agnihotri <niteshagni1830@gmail.com>
2022-06-27 18:46:25 +0530

    update rank

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table } from 'react-bootstrap';
+import './App.css';
 
 export default function App() {
 
@@ -27,27 +28,29 @@ export default function App() {
     }
 
     callData();
-  }, []);
+  });
 
   return (
-    <div className='text-center w-full'>
-      <h1 className='text-center py-5 mx-2'>100 DAYS OF CODE PROGRESS REPORT</h1>
-      <Table responsive bordered className='container my-2 overflow-scroll w-100 '>
+    <div className='text-center w-100'>
+      <h1 className='text-center py-4 mx-2 lh-base lh-sm-0 fs-2'>100 DAYS OF CODE <br/> PROGRESS REPORT</h1>
+      <Table responsive bordered className='container w-auto table-xl text-nowrap my-2 overflow-scroll  table-fixed mx-4 mx-sm-auto '>
         <thead className=''>
-          <tr>
-            <th>NAME</th>
-            <th>College NAME</th>
-            <th>DAYS COMPLETED</th>
-            <th>DAYS MISSED</th>
-            <th>CURRENT STREAK</th>
+          <tr className='text-uppercase table-success'>
+            <th className='px-3'>#</th>
+            <th className='px-3'>NAME</th>
+            <th className='text-nowrap'>College NAME</th>
+            <th className='text-nowrap'>DAYS COMPLETED</th>
+            <th className='text-nowrap'>DAYS MISSED</th>
+            <th className='text-nowrap'>CURRENT STREAK</th>
           </tr>
         </thead>
         <tbody>
           {
             [...user]
-            .sort((a, b) => a.SCORES - b.SCORES)
+            .sort((a, b) => b.CURRENTSTREAK - a.CURRENTSTREAK)
             .map((val, key) => {
-              return <tr key={key} >
+              return <tr key={val.SNO} >
+                <td>{key+1}</td>
                 <td>{val.NAME}</td>
                 <td>{val.COLLEGENAME}</td>
                 <td>{val.DAYSCOMPLETED}</td>
